80fab8100120ba33bdb5feb305a8d7a5c28f59b3
Lorenzo Fiorini <lorenfiorini@gmail.com>
2022-07-14 19:34:01 +0200

    models

--- a_0/test0.py
+++ a_0/test0.py
@@ -3,4 +3,9 @@ import openai
 
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
+a = openai.Model.list()
+
+f = open("models.txt", "w")
+f.write(str(a))
+f.close()
+
